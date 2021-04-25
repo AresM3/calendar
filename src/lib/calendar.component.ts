bf70d03a740c04f342ff56baeb7e6f989b73a243
AresM3 <ares.bagatin@m3team.it>
2021-04-25 17:31:44 +0200

    Prima versione di dev

--- src/lib/calendar.component.ts
+++ src/lib/calendar.component.ts
@@ -1,20 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ViewChild, Input, Output, OnDestroy} from '@angular/core';
+import {Calendar} from '@fullcalendar/core';
+import {EventSourceInput} from '@fullcalendar/common';
+import {CalendarOptions, FullCalendarComponent} from '@fullcalendar/angular';
+import {Observable, Subject, Subscription} from "rxjs";
+import {filter} from "rxjs/operators";
 
 @Component({
-  selector: 'lib-calendar',
-  template: `
-    <p>
-      calendar works!
-    </p>
-  `,
-  styles: [
-  ]
+    selector: 'm3team-calendar',
+    template: `
+        <full-calendar [options]="options"></full-calendar>`,
+    styles  : []
 })
-export class CalendarComponent implements OnInit {
+export class CalendarComponent implements OnDestroy {
+    subscriptions: Subscription[] = [];
+    @ViewChild(FullCalendarComponent) calendarObject: FullCalendarComponent;
 
-  constructor() { }
+    get calendar(): Calendar {return this.calendarObject.getApi()};
 
-  ngOnInit(): void {
-  }
+    @Input('eventClick') eventClicked: (info) => void;
+    @Input('datesSet') datesSet: (arg) => void;
+    changeSubject: Subject<void> = new Subject<void>();
+    @Output('change') change: Observable<void> = this.changeSubject.asObservable();
+    private _source: EventSourceInput = {events: []}
+    get source(): EventSourceInput {return this._source;}
+
+    set source(e) {
+        this._source = e;
+        this.changeSubject.next();
+    }
+
+    options: CalendarOptions = {
+        initialView     : 'timeGridWeek',
+        height          : '100%',
+        locale          : 'it',
+        displayEventEnd : true,
+        slotEventOverlap: false,
+        firstDay        : 1,
+        headerToolbar   : {
+            start : 'dayGridMonth,timeGridWeek,timeGridDay',
+            center: 'title',
+            end   : 'today prev,next'
+        },
+        buttonText      : {
+            today: 'Oggi',
+            month: 'Mese',
+            week : 'Settimana',
+            day  : 'Giorno'
+        }
+    };
+
+    constructor() {
+        this.options.eventClick = this.eventClicked;
+        this.subscriptions.push(this.change.pipe(filter(() => !!this.calendar)).subscribe(() => {
+            this.calendar.removeAllEventSources();
+            this.calendar.addEventSource(this._source);
+        }));
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.forEach(sb => sb.unsubscribe());
+    }
 
 }
--- src/lib/calendar.module.ts
+++ src/lib/calendar.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CalendarComponent } from './calendar.component';
+import {FullCalendarModule} from "@fullcalendar/angular";
 
 
 
 @NgModule({
   declarations: [CalendarComponent],
-  imports: [
-  ],
+  imports: [FullCalendarModule],
   exports: [CalendarComponent]
 })
 export class CalendarModule { }
